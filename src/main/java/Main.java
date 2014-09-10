d4beeb333051f0e1e075d63cd0ddd9f504905b44
Chris Beams <chris@beams.io>
2014-09-09 21:57:38 -0500

    Generate vanity address containing a target string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.params.MainNetParams;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Main {
 
+    private static final NetworkParameters MAINNET = MainNetParams.get();
+    private static final int NUM_PROCS = Runtime.getRuntime().availableProcessors();
+    private static final String TARGET = "winner";
+
     public static void main(String... args) {
-        System.out.println("Shall we play a game?");
+        AtomicBoolean keepSearching = new AtomicBoolean(true);
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_PROCS);
+
+        for (int i = 0; i < NUM_PROCS; i++) {
+            executor.execute(() -> {
+                while (keepSearching.get()) {
+                    ECKey key = new ECKey();
+                    Address address = key.toAddress(MAINNET);
+                    if (address.toString().contains(TARGET)
+                            && keepSearching.compareAndSet(true, false)) {
+                        System.out.println("Address: " + address);
+                        System.out.println("Privkey: " + key.getPrivateKeyEncoded(MAINNET));
+                    }
+                }
+            });
+        }
+
+        executor.shutdown();
     }
 }
