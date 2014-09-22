bf0807065509813b886f479f60a4c3095ed4423f
Chris Beams <chris@beams.io>
2014-09-22 15:42:47 +0200

    Compare Stream-based vs. manual concurrency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,62 @@ import com.google.bitcoin.core.Address;
 import com.google.bitcoin.core.ECKey;
 import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.params.MainNetParams;
+import com.google.common.base.Stopwatch;
 
+import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 public class Main {
-
     private static final NetworkParameters MAINNET = MainNetParams.get();
-    private static final int NUM_PROCS = Runtime.getRuntime().availableProcessors();
-    private static final String TARGET = "1s2gx";
+    private static final String TARGET = "1s2";
 
-    public static void main(String... args) {
+    static {
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "4");
+    }
+
+    public static void main(String... args) throws InterruptedException, IOException {
+        System.out.println("Hit return to start");
+        System.in.read();
+
+        Stopwatch timer = Stopwatch.createStarted();
+
+        /*
+        System.out.println("imperative:");
+        findImperative();
+        System.out.println(timer);
+
+        timer.reset();
+        timer.start();
+        */
+
+        System.out.println("stream:");
+        findWithStream();
+        System.out.println(timer);
+    }
+
+    public static void findWithStream() {
+        Optional<ECKey> k =
+                Stream.generate(ECKey::new)
+                        .parallel()
+                        .filter(key -> key.toAddress(MAINNET).toString().startsWith(TARGET))
+                        .findAny();
+
+        System.out.println("Address: " + k.get().toAddress(MAINNET));
+        System.out.println("Privkey: " + k.get().getPrivateKeyEncoded(MAINNET));
+    }
+
+
+    public static void findImperative() throws InterruptedException {
+        int numProcs = Runtime.getRuntime().availableProcessors();
         AtomicBoolean keepSearching = new AtomicBoolean(true);
-        ExecutorService executor = Executors.newFixedThreadPool(NUM_PROCS);
+        ExecutorService executor = Executors.newFixedThreadPool(numProcs);
 
-        System.out.println("searching for address starting with " + TARGET + "...");
-
-        for (int i = 0; i < NUM_PROCS; i++) {
+        for (int i = 0; i < numProcs; i++) {
             executor.execute(() -> {
                 while (keepSearching.get()) {
                     ECKey key = new ECKey();
@@ -34,5 +72,6 @@ public class Main {
         }
 
         executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
 }
